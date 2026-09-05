48937ada4c7dab6990dc6bde40f2603bd9e646f4
Débora Fraga <deborafragamoreira@gmail.com>
2026-09-04 22:18:16 -0300

    multiplos recyclerview

--- Rango_na_Regua/RangoFile_Start/app/src/main/java/br/edu/unisenai/rangonaregua/MainActivity.java
+++ Rango_na_Regua/RangoFile_Start/app/src/main/java/br/edu/unisenai/rangonaregua/MainActivity.java
@@ -11,16 +11,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.unisenai.rangonaregua.adapter.LugarAdapter;
+import br.edu.unisenai.rangonaregua.data.Catalogo;
+import br.edu.unisenai.rangonaregua.model.Lugar;
 
-public class MainActivity extends AppCompatActivity {
 
-    static List<String> minhaLista = new ArrayList<>();
+public class MainActivity extends AppCompatActivity implements LugarAdapter.Acao {
+
+    static List<Lugar> listaLugar = new ArrayList<>();
+
+    LugarAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +40,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Carregar o Database
+        listaLugar = Catalogo.inicial();
 
         FloatingActionButton btNovo = findViewById(R.id.fabNovo);
         btNovo.setOnClickListener(v -> {
             Intent intent = new Intent(this, NovoLugarActivity.class);
             startActivity(intent);
         });
+
+        //Carregar o recycle
+        RecyclerView rvLugares = findViewById(R.id.rvLugares);
+        rvLugares.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new LugarAdapter(listaLugar, this);
+        rvLugares.setAdapter(adapter);
+
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void votar(Lugar lugar){
+        lugar.setVotos(lugar.getVotos() +1);
+
+        Catalogo.ordenarPorVotos(listaLugar);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void detalhes(Lugar lugar){
+        Intent rota = new Intent(this, DetalheActivity.class);
+        rota.putExtra("obj", lugar);
+        startActivity(rota);
+
+    }
 }
